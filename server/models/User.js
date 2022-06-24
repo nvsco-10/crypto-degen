5e175550998cc378d60fa722702914d091d1a48c
Neema Velasco <neemavelasco@gmail.com>
2022-06-24 08:26:17 -0500

    set up server packages, user model structure

---
+++ server/models/User.js
@@ -0,0 +1,15 @@
+// user model
+
+// username
+// email
+// password
+// portfolio : [coinSchema]
+
+// not a mongoose model
+// coinSchema
+// coinId:  // id from coingecko
+// name: //
+// symbol: //
+// image //
+// qty: default 0
+// 
